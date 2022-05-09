98429dea84ff26490c77b048b46b5c7674298251
Andrey Pechennikov <ap@devandy.me>
2022-05-09 15:38:38 +0300

    Добавил проверку загрузки контента и анимацию футера

--- source/js/script.js
+++ source/js/script.js
@@ -8,6 +8,7 @@ import result from './modules/result.js';
 import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
+import general from './modules/general';
 
 // init modules
 mobileHeight();
@@ -18,6 +19,7 @@ chat();
 result();
 form();
 social();
+general();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
